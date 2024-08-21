e9052b8f9e927b3237ef5228150b4e0eaa6788d9
devwasabi2 <Wandile.Nyembe@bbd.co.za>
2024-08-21 10:12:32 +0200

    fix: Correct the selector for the gameOverlay element

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import game from './loop.js'
 
 document.body.appendChild(game.renderer.domElement)
 //document.getElementById('startOverlay').style.display = 'none';
-document.getElementById('gameOverlay').style.display = 'block';
+document.body.getElementById('gameOverlay').style.display = 'block';
